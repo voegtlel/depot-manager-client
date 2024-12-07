565586d7d824a8564357e55aff01664682181a77
Titouan <larnicol.titouan@orange.fr>
2024-12-07 19:53:13 +0100

    feat: add mock API and Auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { APP_BASE_HREF } from '@angular/common';
@@ -12,8 +12,13 @@ import { DeviceAppRoutingModule } from './device-module/app.routing.device.modul
 import { environment } from 'src/environments/environment';
 import { ClientModuleModule } from './client-module/client-module.module';
 import { DeviceModuleModule } from './device-module/device-module.module';
-import { NbSidebarModule, NbSidebarService, NbThemeModule } from '@nebular/theme';
+import { NbSidebarModule, NbSidebarService, NbThemeModule, NbToastrService } from '@nebular/theme';
 import { ReservationResolver } from './device-module/_services/reservation.resolver';
+import { AuthMockService } from './common-module/_services/auth-mock.service';
+import { ApiService, AuthService, EnvService } from './common-module/_services';
+import { ApiMockService } from './common-module/_services/api-mock.service';
+import { OAuthService } from 'angular-oauth2-oidc';
+import { Router } from '@angular/router';
 
 @NgModule({
     // entryComponents: [CalendarRangeDayCellComponent, CalendarRangeComponent],
@@ -27,7 +32,30 @@ import { ReservationResolver } from './device-module/_services/reservation.resol
         environment.onDevice ? DeviceAppRoutingModule : AppRoutingModule,
         NbSidebarModule,
     ],
-    providers: [NbSidebarService, { provide: APP_BASE_HREF, useValue: '/' }, ReservationResolver],
+    providers: [NbSidebarService, { provide: APP_BASE_HREF, useValue: '/' }, ReservationResolver,
+        {
+            provide: AuthService,
+            useFactory: (oAuth: OAuthService, envService: EnvService,router: Router, toastService: NbToastrService) => {
+                if (environment.offline) {
+                    return new AuthMockService(router);
+                } else {
+                    return new AuthService(oAuth, envService,router, toastService);
+                }
+            },
+            deps: [OAuthService, EnvService, Router, NbToastrService]
+            },
+        {
+            provide: ApiService,
+            useFactory: (httpClient: HttpClient, envService: EnvService,authService: AuthService) => {
+                if (environment.offline) {
+                    return new ApiMockService(authService);
+                } else {
+                    return new ApiService( httpClient,envService, authService);
+                }
+            },
+            deps: [HttpClient, EnvService, OAuthService]
+        },
+        ],
 
     bootstrap: [AppComponent],
 })
--- src/environments/environment.device.ts
+++ src/environments/environment.device.ts
@@ -6,4 +6,5 @@ export const environment = {
     production: false,
     onDevice: true,
     appBaseHref: '/',
+    offline: false,
 };
