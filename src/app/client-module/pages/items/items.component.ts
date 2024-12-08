3c4497ce179fe729507ceaa148f3c9f28dd584dd
Johannes Gest <kiiu.rib@gmail.com>
2024-12-08 02:20:25 +0100

    feat: Tiny little improvement for items table

--- src/app/client-module/pages/items/items.component.ts
+++ src/app/client-module/pages/items/items.component.ts
@@ -41,16 +41,15 @@ export class ItemsComponent implements OnInit, OnDestroy {
 
     allColumns = [
         'externalId',
+        'tags',
+        'picture',
         'name',
         'description',
         'conditionText',
         'purchaseDate',
         'lastService',
         'bay',
-        'picture',
-        'tags',
-        'action',
-        'action-details',
+        'actions',
     ];
 
     dataSource: NbTreeGridDataSource<ItemEntry> = null;
